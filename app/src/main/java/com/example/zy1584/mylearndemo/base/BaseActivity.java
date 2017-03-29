9ca30874564365a246e034a54bdcfea543555762
tangzhaoqiang <tangzhaoqiang@doov.com.cn>
2017-03-29 18:51:01 +0800

    增加了6.0权限封装

--- app/src/main/java/com/example/zy1584/mylearndemo/base/BaseActivity.java
+++ app/src/main/java/com/example/zy1584/mylearndemo/base/BaseActivity.java
@@ -1,14 +1,23 @@
 package com.example.zy1584.mylearndemo.base;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.CallSuper;
 import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -25,6 +34,9 @@ import com.trello.rxlifecycle.LifecycleTransformer;
 import com.trello.rxlifecycle.RxLifecycle;
 import com.trello.rxlifecycle.android.ActivityEvent;
 import com.trello.rxlifecycle.android.RxLifecycleAndroid;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import rx.Observable;
@@ -220,6 +232,131 @@ public abstract class BaseActivity<P extends BasePresenter> extends AppCompatAct
      */
     public void LogI(String str) {
         Logger.t(TAG).i(str);
+    }
+
+    /******************************************* Android 6.0权限封装 ***********************************************/
+
+    public static  int REQUEST_CODE = 0;
+
+    public void requestPermission(String[] permissions,int requestCode)
+    {
+
+        this.REQUEST_CODE = requestCode;
+
+        //检查权限是否授权
+        if(checkPermissions(permissions))
+        {
+            permissionSucceed(REQUEST_CODE);
+        }
+        else
+        {
+            List<String> needPermissions = getPermissions(permissions);
+            ActivityCompat.requestPermissions(this, needPermissions.toArray(new String[needPermissions.size()]), REQUEST_CODE);
+        }
+    }
+
+    /**
+     * 检测所有的权限是否都已授权
+     *
+     * @param permissions
+     * @return
+     */
+    private boolean checkPermissions(String[] permissions)
+    {
+        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M)
+        {
+            return true;
+        }
+
+        for(String permission:permissions)
+        {
+
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<String> getPermissions(String[] permissions)
+    {
+        List<String> permissionList = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) !=
+                    PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                permissionList.add(permission);
+            }
+        }
+        return permissionList;
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        if (requestCode == REQUEST_CODE) {
+            if (verificationPermissions(grantResults)) {
+                permissionSucceed(REQUEST_CODE);
+            } else {
+                permissionFailing(REQUEST_CODE);
+                showFailingDialog();
+            }
+        }
+    }
+
+    private boolean verificationPermissions(int[] results)
+    {
+
+        for (int result : results) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    private void showFailingDialog()
+    {
+
+        new AlertDialog.Builder(this)
+                .setTitle("消息")
+                .setMessage("当前应用无此权限，该功能暂时无法使用。如若需要，请单击确定按钮进行权限授权！")
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        return;
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startSettings();
+                    }
+                }).show();
+
+    }
+
+    private void startSettings() {
+
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        startActivity(intent);
+    }
+
+    public void permissionFailing(int code) {
+
+        Log.d(TAG, "获取权限失败=" + code);
+    }
+
+    public void permissionSucceed(int code) {
+
+        Log.d(TAG, "获取权限成功=" + code);
     }
 
     /******************************************* RXActivity ***********************************************/
